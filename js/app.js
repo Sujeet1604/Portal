f6a6757b76dd180844875b2aab1d8f6d918fda8f
Sujeet1604 <sujeet161190@gmail.com>
2016-01-06 04:33:01 +0530

    change 001
    css changes

--- js/app.js
+++ js/app.js
@@ -63,7 +63,8 @@ $(document).ready(function(){
     var jumboHeight = $('.jumbotron').outerHeight();
 function parallax(){
     var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    $('.bg').css('height', (jumboHeight-scrolled) +110+ 'px');
+    $('.jumbotron').css('height', (jumboHeight) + 'px');
 }
 
 $(window).scroll(function(e){
